88ea20e242b8422cf8457b7917831309dc675f77
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-07-20 19:37:54 -0300

    img gabinete

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToLink() {
+    var linkpm = "https://www.asrock.com/mb/AMD/B550M%20Pro%20SE/index.br.asp"
+    
+    document.location.href = linkpm
+}
+
+function redirectToLink() {
+    var linkpm = "https://www.mercadolivre.com.br/gabinete-gamer-aigo-c285-argb-mid-t-lat-de-vidro-c-4f-w-cor-branco/p/MLB26849826#reviews"
+    
+    document.location.href = linkpm
+}
