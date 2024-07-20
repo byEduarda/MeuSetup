0ebdfe79d2ec293a2e4c0777c1dc3ea5fbf9dfa4
Maria Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2024-07-20 19:48:50 -0300

    correção link

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function redirectToLink() {
+function redirectToLinkpm() {
     var linkpm = "https://pt.aliexpress.com/item/1005005980078919.html"
     
     document.location.href = linkpm
 }
 
-function redirectToLink() {
+function redirectToLinkg() {
     var linkpm = "https://www.mercadolivre.com.br/gabinete-gamer-aigo-c285-argb-mid-t-lat-de-vidro-c-4f-w-cor-branco/p/MLB26849826#reviews"
     
     document.location.href = linkpm
